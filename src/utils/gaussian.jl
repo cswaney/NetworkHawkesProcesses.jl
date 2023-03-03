5ecfa47b1c4282f153db668713ba0b45e855d18f
Colin Swaney <colinswaney@princeton.edu>
2023-03-03 11:49:08 -0500

    Kernels apply to abstract vector types

--- src/utils/gaussian.jl
+++ src/utils/gaussian.jl
@@ -5,7 +5,7 @@ abstract type Kernel end
 
 function (kernel::Kernel)(x, y) end
 
-function (kernel::Kernel)(x::Vector)
+function (kernel::Kernel)(x::AbstractVector)
     n = length(x)
     Σ = zeros(n, n)
     for i = 1:n
