75a4b822ef2196eb618e8e1f7e624c29129bfe7b
Colin Swaney <colinswaney@princeton.edu>
2023-05-02 20:06:06 -0400

    Use multiple tries to make posdef

--- src/utils/helpers.jl
+++ src/utils/helpers.jl
@@ -1,8 +1,12 @@
-function posdef!(sigma)
-    eps = 2 * minimum(eigvals(sigma))
-    eps > 0.0 && return sigma
-    idx = diagind(sigma)
-    sigma[idx] .-= eps
+function posdef!(sigma; maxiter=3)
+    iter = 0
+    while iter < maxiter
+        eps = 2 * minimum(eigvals(sigma))
+        eps > 0.0 && return sigma
+        sigma[diagind(sigma)] .-= eps
+        iter += 1
+    end
+    println("WARNING: failed to make sigma positive definite")
     return sigma
 end
 
