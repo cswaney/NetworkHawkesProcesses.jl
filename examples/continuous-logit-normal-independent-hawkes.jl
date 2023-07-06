708f4d13dd59d344be2f09b7156642c0f5a4fe96
Colin Swaney <colinswaney@princeton.edu>
2023-07-06 10:13:11 -0400

    Create continuous-logit-normal-independent-hawkes.jl

---
+++ examples/continuous-logit-normal-independent-hawkes.jl
@@ -0,0 +1,42 @@
+"""A simple synthetic example using a continuous-time standard process with logit-normal impulse response."""
+
+using NetworkHawkesProcesses
+using NetworkHawkesProcesses: params, params!
+using Statistics
+using Random
+using LinearAlgebra: Diagonal
+
+# set random seed
+Random.seed!(0);
+
+# set hyperparameters
+nnodes = 2;
+duration = 100.0; # 100.0 or 1000.0
+Δtmax = 1.0;
+
+# create a random process
+baseline = HomogeneousProcess(rand(nnodes));
+weights = IndependentWeightModel(Diagonal(rand(nnodes)));
+impulses = LogitNormalImpulseResponse(rand(nnodes, nnodes), rand(nnodes, nnodes), Δtmax);
+process = ContinuousStandardHawkesProcess(baseline, impulses, weights);
+println("Process is stable? $(isstable(process))")
+
+# save a copy of parameters
+θ = copy(params(process));
+
+# generate random data
+data = NetworkHawkesProcesses.rand(process, duration);
+println("Generated $(length(data[1])) events")
+
+# estimate parameters via mle
+res = mle!(process, data; verbose=true);
+θmle = res.maximizer;
+[θ θmle]
+
+# reset parameters
+params!(process, θ);
+
+# estimate parameters via mcmc (NOTE: this would be *extremely* slow for exponential impulse response)
+res = mcmc!(process, data; verbose=true);
+θmcmc = mean(res.samples);
+[θ θmcmc]
