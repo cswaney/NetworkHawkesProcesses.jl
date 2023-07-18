045ea445d47d3fe7bfd328eb0d7ef955638bab91
Colin Swaney <colinswaney@princeton.edu>
2023-07-18 11:56:35 -0400

    Create lgcp-logit-normal.jl

---
+++ examples/continuous/univariate/lgcp-logit-normal.jl
@@ -0,0 +1,48 @@
+"""A simple synthetic example using a continuous-time univariate process with logit-normal impulse response and log-Gaussian Cox baseline."""
+
+using NetworkHawkesProcesses
+using NetworkHawkesProcesses: params, params!
+using NetworkHawkesProcesses: SquaredExponentialKernel, GaussianProcess
+using Statistics
+using Random
+
+# set random seed
+Random.seed!(0);
+
+# set hyperparameters
+nnodes = 2;
+duration = 100.0; # 100.0 or 1000.0
+Δtmax = 2.0;
+sigma = 1.0
+eta = 1.0
+bias = 0.0
+nsteps = 10
+
+# create a random process
+kernel = SquaredExponentialKernel(sigma, eta);
+gp = GaussianProcess(kernel);
+baseline = LogGaussianCoxProcess(gp, bias, duration, nsteps, nnodes);
+weights = DenseWeightModel(rand(nnodes, nnodes));
+impulses = LogitNormalImpulseResponse(rand(nnodes, nnodes), rand(nnodes, nnodes), Δtmax);
+process = ContinuousStandardHawkesProcess(baseline, impulses, weights);
+println("Process is stable? $(isstable(process))")
+
+# save a copy of parameters
+θ = copy(params(process));
+
+# generate random data
+data = NetworkHawkesProcesses.rand(process, duration);
+println("Generated $(length(data[1])) events")
+
+# estimate parameters via mle
+res = mle!(process, data; verbose=true);
+θmle = res.maximizer;
+[θ θmle]
+
+# reset parameters
+params!(process, θ);
+
+# estimate parameters via mcmc
+res = mcmc!(process, data; verbose=true);
+θmcmc = mean(res.samples);
+[θ θmcmc]
