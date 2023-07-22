41cc2482675a6be14a87e7a7aa839b5245a2d2d5
Colin Swaney <colinswaney@princeton.edu>
2023-07-21 20:49:42 -0400

    Create lgcp-logit-normal.jl

---
+++ examples/continuous/network/lgcp-logit-normal.jl
@@ -0,0 +1,43 @@
+"""A simple synthetic example using a continuous-time standard process with logit-normal impulse response."""
+
+using NetworkHawkesProcesses
+using NetworkHawkesProcesses: params, params!
+using NetworkHawkesProcesses: SquaredExponentialKernel, GaussianProcess
+using Statistics
+using Random
+
+# set random seed
+Random.seed!(0);
+
+# set hyperparameters
+nnodes = 2;
+duration = 100.0; # 100.0 or 1000.0
+Δtmax = 2.0;
+sigma = 1.0
+eta = 1.0
+bias = 0.0
+nsteps = 10
+plink = 0.5;
+
+# create a random process
+kernel = SquaredExponentialKernel(sigma, eta);
+gp = GaussianProcess(kernel);
+baseline = LogGaussianCoxProcess(gp, bias, duration, nsteps, nnodes);
+weights = DenseWeightModel(rand(nnodes, nnodes));
+impulses = LogitNormalImpulseResponse(rand(nnodes, nnodes), rand(nnodes, nnodes), Δtmax);
+network = NetworkHawkesProcesses.BernoulliNetworkModel(plink, nnodes);
+links = NetworkHawkesProcesses.rand(network);
+process = ContinuousNetworkHawkesProcess(baseline, impulses, weights, links, network);
+println("Process is stable? $(isstable(process))")
+
+# save a copy of parameters
+θ = copy(params(process));
+
+# generate random data
+data = NetworkHawkesProcesses.rand(process, duration);
+println("Generated $(length(data[1])) events")
+
+# estimate parameters via mcmc (mle not available for discrete variables)
+res = mcmc!(process, data; verbose=true);
+θmcmc = mean(res.samples);
+[θ θmcmc]
